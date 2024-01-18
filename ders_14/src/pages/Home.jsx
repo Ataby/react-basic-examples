cd6a886fb5f599821ea42d4c74d9a93d31f45265
Ataby <matabay200069@gmail.com>
2024-01-18 22:35:45 +0300

    create theme

--- ders_14/src/pages/Home.jsx
+++ ders_14/src/pages/Home.jsx
@@ -3,15 +3,28 @@ import   AppBar   from "../components/Appbar";
 import CardGrid from "../components/CardGrid";
 import TextFieldComp from "../components/TextFieldComp";
 import TypoButtons from "../components/TypoButtons";
+import { createTheme,ThemeProvider } from "@mui/material";
 
 const Home = () => {
+  const theme = createTheme({
+    palette:{
+      primary:{
+        main: "#2a1",
+        light: "#7dc973",
+        dark: "#1a7b0d"
+      }
+    }
+  })
+
+
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       {/* <TypoButtons />
       <TextFieldComp/> */}
       <AppBarResponsive/>
       <CardGrid/>
-    </>
+    </ThemeProvider>
   );
 };
 
