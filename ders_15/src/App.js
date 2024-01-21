f041b85557039f93d509463ee7578001a8f3b264
Ataby <matabay200069@gmail.com>
2024-01-21 17:47:58 +0300

    sginIn & signUp

---
+++ ders_15/src/App.js
@@ -0,0 +1,29 @@
+ import './App.css';
+import { useEffect } from 'react';
+import {signIn, signUp} from "./config/firebase" 
+
+function App() {
+//SIGN.UP ASYNC FONKSIYON OLDUGU ICIN '.THEN' ILE BASKA ISLEMLER YAPABILIRIZ.
+  // useEffect(() => {
+  //   signUp ("Michale","michale@mail.com","111222")
+  //   .then(()=> { console.log("done")})
+  //   .catch((error)=>{console.log(error)})
+  
+  // }, [ ])
+
+  useEffect(() => {
+    signIn ("mifdghale@mail.com","221222")
+    .then(()=> { console.log("done")})
+    .catch((error)=>{console.log(error)})
+  
+  }, [ ])
+  
+
+  return (
+   
+      <h1>application!!</h1> 
+ 
+  );
+}
+
+export default App;
---
+++ ders_15/src/config/firebase.jsx
@@ -0,0 +1,28 @@
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics"
+import {getAuth,createUserWithEmailAndPassword,updateCurrentUser,signInWithEmailAndPassword} from "firebase/auth"
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDHHdVRCGOJGBE1CVaN7GMDw-x3pVGXU4s",
+    authDomain: "react-firebase-to-do-list.firebaseapp.com",
+    projectId: "react-firebase-to-do-list",
+    storageBucket: "react-firebase-to-do-list.appspot.com",
+    messagingSenderId: "819568987015",
+    appId: "1:819568987015:web:824cb32bf628e9609e6122",
+    measurementId: "G-2DX3ZVXZCT"
+  };
+
+ const initApp= initializeApp(firebaseConfig);
+  getAnalytics(initApp);
+  const auth = getAuth(initApp); 
+
+  export const signIn = async function(email,passw){
+    await signInWithEmailAndPassword(auth,email,passw);
+  };
+
+  export const signUp = async function(name,email,passw){
+    await createUserWithEmailAndPassword(auth,email,passw);
+    await updateCurrentUser(auth,{displayName:name});
+    //PROMISE DONDURECEGI ICIN MI 'AWAIT' EKLEDIK ? 
+  }
