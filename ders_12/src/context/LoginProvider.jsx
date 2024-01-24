93b02e3a7ec326095d6bb326f0a19891836cb7ee
Ataby <matabay200069@gmail.com>
2024-01-24 17:34:07 +0300

    done

--- ders_12/src/context/LoginProvider.jsx
+++ ders_12/src/context/LoginProvider.jsx
@@ -6,7 +6,7 @@ export const LoginContext = createContext();
 
 // 2- Providing: Dosyanın adı 'Provider' iceriyorsa Context dosyasıdır.
 const LoginProvider = ({children}) => {
-  //DEFAULT OLARAK HER PROVIDER'DA OLAN OZELLIK
+  //'children' DEFAULT OLARAK HER PROVIDER'DA OLAN OZELLIK
   // VEYA (props.children) SEKLINDE YAZILABILIR.
   const [user,setuser]=useState("");  
   const values= {user,setuser};            
@@ -19,7 +19,7 @@ const LoginProvider = ({children}) => {
 
 export default LoginProvider;
 
-// 3- Yazdığımız bir hook, başka bir hokk'u çağırıyorsa buna "custom hook" denir. Mutlaka 'use' ile başlaması gerekir.
+// 3- Consuming: Yazdığımız bir hook, başka bir hokk'u çağırıyorsa buna "custom hook" denir. Mutlaka 'use' ile başlaması gerekir.
 export const useLoginContext =()=>{
   return useContext(LoginContext);
 }
