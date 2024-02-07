c14414449203974780eea40c22ff4a729bb673e4
Ataby <matabay200069@gmail.com>
2024-02-07 15:48:22 +0300

    fetch & useEffect & useNavigate

--- ders_12/src/pages/PersonDetail.jsx
+++ ders_12/src/pages/PersonDetail.jsx
@@ -1,65 +1,59 @@
-import { useParams, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
+import React from 'react'
+import { useParams,useLocation,useNavigate } from 'react-router-dom'
+import {useState,useEffect} from "react";
+import NotFound from './NotFound';
+//PERSON DETAIL'IN YANI BIR SAYFADA AYRINTILARINI GOSTERMEK ISTEDIGIMIZ ICIN PAGES ACTIK. EGER AYNI SAYFADA GOSTERMEK ISTESEYDIK COMPONENT ICINE YERLESTIRIRDIK.
 
 const PersonDetail = () => {
-  const navigate = useNavigate();
+    const navigate =useNavigate();
+    const {id}=useParams(); // TIKLANAN ITEM ICIN YENI SAYFA AC.
+    const [person, setPerson] = useState("");
+    const [error, seterror] = useState(false)
+    console.log(id);
+    
+    // const {state: person} = useLocation();// TIKLANAN ITEM BILGILERINI BU SAYFAYA AKTAR. FAKAT ON.CLICK EVENT'I ILE YONLENDIRDIGI ICIN DIREK LINK ADRESINI GIREREK BURAYA ULASAMAYIZ.
+    // console.log(person);
+ 
+    const getPerson = () => {
+        fetch(`https://reqres.in/api/users/${id}`)
+          .then((res) =>{
+          if(!res.ok){
+            seterror(true)
+            throw new Error("Something went wrong.");
+            //THROW'DAN SONRA DIREK 'CATCH'  KISMINA ATLAYACAGI ICIN 'SET' METODU USTE YAZILDI.
+          }
+          //SUSLU PARANTEZ ACILDIGI ICIN AUTO RETURN KAYBOLUR.
+          return res.json();})
+          .then((data) => setPerson(data.data))
+          .catch((err) => console.log(err));
+      };
+      // FONKSIYONU TANIMLA, FETCH ILE VERIYI AL, JSON FORMATINA CEVIR, STATE DEGERINE AKTAR, USE.EFFECT ILE FONKSIYONUNU BASLANGICTA CALISTIR, GUNCELLEME ICIN DEPENDENCY VARIABLE EKLEYEBILIRSIN.
 
-  const { id } = useParams();
-  const [person, setPerson] = useState(null);
-  const [error, setError] = useState(false);
+      useEffect(() => {
+        getPerson();
+      }, []);
 
-  useEffect(() => {
-    fetch(`https://reqres.in/api/users/${id}`)
-      .then((res) => {
-        if (!res.ok) {
-          setError(true);
-          throw new Error("Something went wrong");
-        }
-        return res.json();
-      })
-      .then((data) => setPerson(data.data))
-      .catch((err) => console.log(err));
-  }, [id]);
-
-  if (error) {
-    return <h1>Page Not Found</h1>;
-  } else if (!person) {
-    return (
-      <div className="text-center">
-        <h2>Data is Fetching</h2>
+  if (error){
+    return <NotFound/>; //JSX'DE COMPONENTIN MUHAKKAK "RETURN" DONDURMESI GEREKIR.
+  }else if (!person) { //EGER ERROR YOKSA AMA 'PERSON' OBJESI HENUZ BOS ISE 'LOADING MESAJI GOSTER.'
+      return <div className='text-center'>
+        <h3> Data Loading...</h3>
       </div>
-    );
-  } else {
+  }else{
     return (
-      <Container className="text-center">
+      <div className='container text-center text-danger bg-light p-3'>
+        <img src={person.avatar} alt="" className='rounded'/>
         <h3>
-          {person?.first_name} {person?.last_name}
+            {person.first_name} {person.last_name}
         </h3>
-        <Image
-          className="w-50 rounded shadow-lg"
-          src={person?.avatar}
-          alt="img"
-        />
-        <h4>{person.email}</h4>
-
-        <Container>
-          <Button
-            onClick={() => navigate("/")}
-            variant="success"
-            className="me-2"
-          >
-            Go Home
-          </Button>
-          <Button onClick={() => navigate(-1)} variant="warning">
-            Go Back
-          </Button>
-        </Container>
-      </Container>
-    );
+        <p>{person.email}</p>
+        <div>
+        <button onClick={()=>navigate("/")} className="btn btn-danger me-3" style={{backgroundColor:"purple"}}>Home</button>
+        <button onClick={()=>navigate(-1)} className="btn btn-warning text-white">Go Back</button>
+      </div>
+    </div>
+    )
   }
-};
+}
 
-export default PersonDetail;
+export default PersonDetail
